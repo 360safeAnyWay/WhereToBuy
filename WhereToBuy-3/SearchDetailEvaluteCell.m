e8ab592ee2b1ffc0100ada2557139c7d609a0ffa
360safeAnyWay <309226884@qq.com>
2015-03-07 11:54:28 +0800

    建立评论cell

---
+++ WhereToBuy-3/SearchDetailEvaluteCell.m
@@ -0,0 +1,23 @@
+//
+//  SearchDetailEvaluteCell.m
+//  WhereToBuy
+//
+//  Created by ywj on 15-3-7.
+//  Copyright (c) 2015年 JingMo. All rights reserved.
+//
+
+#import "SearchDetailEvaluteCell.h"
+
+@implementation SearchDetailEvaluteCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
