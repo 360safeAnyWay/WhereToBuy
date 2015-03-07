db0c0325304e31eb4ff1dd34af0b4ca310acdbff
360safeAnyWay <309226884@qq.com>
2015-03-07 15:46:12 +0800

    新建房屋详细控制器

---
+++ WhereToBuy-3/WhereToBuy/Classes/WhereToBuy/BuilddingDetailViewController.m
@@ -0,0 +1,23 @@
+//
+//  BuilddingDetailViewController.m
+//  WhereToBuy
+//
+//  Created by ywj on 15-3-7.
+//  Copyright (c) 2015年 JingMo. All rights reserved.
+//
+
+#import "BuilddingDetailViewController.h"
+
+@interface BuilddingDetailViewController ()
+
+@end
+
+@implementation BuilddingDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+@end
