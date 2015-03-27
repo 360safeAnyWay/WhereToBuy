3c0c1397643f4292054a62bd2731d850e84daa3b
360safeAnyWay <max-mengfanjie@MAX-MENGFANJIEdeMacBook-Pro.local>
2015-03-27 09:12:44 +0800

    修改颜色

--- WhereToBuy-3/WhereToBuy/Classes/Other/MFTextView.m
+++ WhereToBuy-3/WhereToBuy/Classes/Other/MFTextView.m
@@ -25,6 +25,7 @@
 
 - (void)setPlaceholder:(NSString *)placeholder {
     if (![placeholder isEqual:_placeholder]) {
+        _placeholderTextColor = [UIColor lightGrayColor];
         _placeholder = placeholder;
         [self drawPlaceholder];
     }
@@ -34,6 +35,8 @@
 #pragma mark - 父类方法
 - (id)initWithCoder:(NSCoder *)aDecoder {
     if (self = [super initWithCoder:aDecoder]) {
+        _placeholderTextColor = [UIColor lightGrayColor];
+
         [self configureBase];
     }
     return self;
@@ -41,6 +44,8 @@
 
 - (id)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
+        _placeholderTextColor = [UIColor lightGrayColor];
+
         [self configureBase];
     }
     return self;
@@ -49,16 +54,19 @@
 - (void)drawRect:(CGRect)rect {
     [super drawRect:rect];
     if (_shouldDrawPlaceholder) {
-        [_placeholderTextColor set];
+        [_placeholderTextColor  set];
 //        [_placeholder drawInRect:CGRectMake(8.0f, 8.0f, self.frame.size.width - 16.0f,
 //                                            self.frame.size.height - 16.0f) withFont:self.font];
         [_placeholder drawInRect:CGRectMake(8.0f, 8.0f, self.frame.size.width - 16.0f,
-                                            self.frame.size.height - 16.0f)  withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:18.0f]}];
+                                            self.frame.size.height - 16.0f)  withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:18.0f],NSForegroundColorAttributeName:kMainTextColor}];
+    self.placeholderTextColor= [UIColor colorWithWhite:0.702f alpha:1.0f];
+     
+
     }
     return;
 }
-
 - (void)configureBase {
+
     [[NSNotificationCenter defaultCenter] addObserver:self
                             selector:@selector(textChanged:)
                                 name:UITextViewTextDidChangeNotification
@@ -66,13 +74,14 @@
     
     self.placeholderTextColor = [UIColor lightGrayColor];
     _shouldDrawPlaceholder = NO;
+    _placeholderTextColor = [UIColor lightGrayColor];
     return;
 }
 
 - (void)drawPlaceholder {
     BOOL prev = _shouldDrawPlaceholder;
     _shouldDrawPlaceholder = self.placeholder && self.placeholderTextColor && self.text.length == 0;
-    
+    _placeholderTextColor = [UIColor lightGrayColor];
     if (prev != _shouldDrawPlaceholder) {
         [self setNeedsDisplay];
     }
@@ -80,6 +89,8 @@
 }
 
 - (void)textChanged:(NSNotification *)notification {
+    _placeholderTextColor = [UIColor lightGrayColor];
+
     [self drawPlaceholder];
     return;
 }
