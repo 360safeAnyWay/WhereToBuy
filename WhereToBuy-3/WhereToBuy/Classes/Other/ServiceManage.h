2fb9386093137aab1f09c31c118c3e15eb03cd01
360safeAnyWay <309226884@qq.com>
2015-04-02 11:02:57 +0800

    完成登录接口

--- WhereToBuy-3/WhereToBuy/Classes/Other/ServiceManage.h
+++ WhereToBuy-3/WhereToBuy/Classes/Other/ServiceManage.h
@@ -10,48 +10,48 @@ typedef void(^DATAARRAY)(NSMutableArray  * array,NSString       * error);
 #import <Foundation/Foundation.h>
 
 typedef enum {
-    ERROR_CODE_NONE = 0,        //成功
-    ERROR_CODE_RROR,
+    ERROR_CODE_NONE = 10000001,        //成功
     ERROR_CODE_RequestFailed,   //连接失败或请求超时
+    ERROR_CODE_RROR,//参数错误
 }ERROR_CODE;
 
 @interface ServiceManage : NSObject
 
 +(instancetype)shareInstance;
 
--(void)DidConnectServer:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidConnectServer:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 
 //获取城市
--(void)DidGetCity:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidGetCity:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 
 //获取价格
--(void)DidGetPrice:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidGetPrice:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 
 //获取面积
--(void)DidGetArea:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidGetArea:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 
 //获取房型
--(void)DidGetType:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidGetType:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 
 //获取城市下面区域
--(void)DidGetDistrict:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidGetDistrict:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 
 //请求房产数据
--(void)DidGetHousesName:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidGetHousesName:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 
 //登录
--(void)DidLogin:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidLogin:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 
 //获取验证码
--(void)DidRequestToken:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidRequestToken:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 //验证验证码信息
--(void)DidRerequestToken:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidRerequestToken:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 
 //注册
--(void)DidRegister:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidRegister:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 
 //获取用户个人信息
--(void)DidUserInfo:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack;
+-(void)DidUserInfo:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack;
 
 
 @end
--- WhereToBuy-3/WhereToBuy/Classes/Other/ServiceManage.m
+++ WhereToBuy-3/WhereToBuy/Classes/Other/ServiceManage.m
@@ -66,7 +66,7 @@
 
 
 //链接服务器得测试
--(void)DidConnectServer:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidConnectServer:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
     [self requestMethod:@"PUT" serviceName:@"/api.php/user/144" parmers:dic completeBlock:^(id obj) {
@@ -74,160 +74,161 @@
         }];
 }
 //获取城市
--(void)DidGetCity:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidGetCity:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
     [dic setObject:@"HePd443Mnd" forKey:@"overifyname"];
     [self requestMethod:@"GET" serviceName:@"/cpanel/index.php/buyc_interface1/getCity/" parmers:dic completeBlock:^(id obj) {
-        ERROR_CODE code = ERROR_CODE_NONE;
+        ERROR_CODE status = ERROR_CODE_RequestFailed;
         if (obj && obj[@"data"]) {
-            code = [obj[@"code"] intValue];
+            status = [obj[@"status"] intValue];
         }
-        callBack(code, obj[@"data"]);
+        callBack(status, obj[@"data"]);
     }];
 }
 
 //获取价格
--(void)DidGetPrice:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidGetPrice:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
     [dic setObject:@"HePd443Mnd" forKey:@"overifyname"];
     [self requestMethod:@"GET" serviceName:@"/cpanel/index.php/buyc_interface1/getLMprice/" parmers:dic completeBlock:^(id obj) {
-        ERROR_CODE code = ERROR_CODE_RROR;
+        ERROR_CODE status = ERROR_CODE_RequestFailed;
         if (obj && obj[@"data"]) {
-            code = [obj[@"code"] intValue];
+            status = [obj[@"status"] intValue];
         }
-        callBack(code, obj[@"data"]);
+        callBack(status, obj[@"data"]);
     }];
 }
 
 //获取面积
--(void)DidGetArea:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidGetArea:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
     [dic setObject:@"HePd443Mnd" forKey:@"overifyname"];
     [self requestMethod:@"GET" serviceName:@"/cpanel/index.php/buyc_interface1/getLMarea/" parmers:dic completeBlock:^(id obj) {
-        ERROR_CODE code = ERROR_CODE_RROR;
+        ERROR_CODE status = ERROR_CODE_RequestFailed;
         if (obj && obj[@"data"]) {
-            code = [obj[@"code"] intValue];
+            status = [obj[@"code"] intValue];
         }
-        callBack(code, obj[@"data"]);
+        callBack(status, obj[@"data"]);
     }];
 }
 
 //获取房型
--(void)DidGetType:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidGetType:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
     [dic setObject:@"HePd443Mnd" forKey:@"overifyname"];
     [self requestMethod:@"GET" serviceName:@"/cpanel/index.php/buyc_interface1/getLMtype/" parmers:dic completeBlock:^(id obj) {
-        ERROR_CODE code = ERROR_CODE_RROR;
+        ERROR_CODE status = ERROR_CODE_RequestFailed;
         if (obj && obj[@"data"]) {
-            code = [obj[@"code"] intValue];
+            status = [obj[@"status"] intValue];
         }
-        callBack(code, obj[@"data"]);
+        callBack(status, obj[@"data"]);
     }];
 }
 
 //获取城市下面区域
--(void)DidGetDistrict:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidGetDistrict:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
     [dic setObject:@"HePd443Mnd" forKey:@"overifyname"];
     [self requestMethod:@"GET" serviceName:@"/cpanel/index.php/buyc_interface1/getDistrict/" parmers:dic completeBlock:^(id obj) {
-        ERROR_CODE code = ERROR_CODE_RROR;
+        ERROR_CODE status = ERROR_CODE_RequestFailed;
         if (obj && obj[@"data"]) {
-            code = [obj[@"code"] intValue];
+            status = [obj[@"status"] intValue];
         }
-        callBack(code, obj[@"data"]);
+        callBack(status, obj[@"data"]);
     }];
 }
 
 //请求房产数据
--(void)DidGetHousesName:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidGetHousesName:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
     [dic setObject:@"HePd443Mnd" forKey:@"overifyname"];
     [self requestMethod:@"GET" serviceName:@"/cpanel/index.php/buyc_interface1/listResAndSub/" parmers:dic completeBlock:^(id obj) {
-        ERROR_CODE code = ERROR_CODE_RROR;
+        ERROR_CODE status = ERROR_CODE_RequestFailed;
         if (obj && obj[@"data"]) {
-            code = [obj[@"code"] intValue];
+            status = [obj[@"status"] intValue];
         }
-        callBack(code, obj[@"data"]);
+        callBack(status, obj[@"data"]);
     }];
 }
 
 //登录
--(void)DidLogin:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidLogin:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
-    [dic setObject:@"HePd443Mnd" forKey:@"overifyname"];
-    [self requestMethod:@"POST" serviceName:@"/cpanel/index.php/buyc_interface1/loginToMainaer/" parmers:dic completeBlock:^(id obj) {
-        ERROR_CODE code = ERROR_CODE_RROR;
+    [dic setObject:@"IOS" forKey:@"client"];
+    [self requestMethod:@"POST" serviceName:@"/api.php/user/login" parmers:dic completeBlock:^(id obj) {
+        ERROR_CODE status = ERROR_CODE_RequestFailed;
         if (obj && obj[@"message"]) {
-            code = [obj[@"code"] intValue];
+            status = [obj[@"status"] intValue];
         }
-        callBack(code, obj);
+        NSLog(@"%@",obj);
+        callBack(status, obj);
     }];
 }
 
 //获取验证码
--(void)DidRequestToken:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidRequestToken:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
     [dic setObject:@"IOS" forKey:@"client"];
     [self requestMethod:@"POST" serviceName:@"/api.php/send/phone" parmers:dic completeBlock:^(id obj) {
-        ERROR_CODE code = ERROR_CODE_RROR;
+        ERROR_CODE status = ERROR_CODE_RequestFailed;
         if (obj && obj[@"message"]) {
-            code = [obj[@"code"] intValue];
+            status = [obj[@"status"] intValue];
         }
         
         NSLog(@"%@",obj);
-        callBack(code, obj);
+        callBack(status, obj);
     }];
 }
 //验证验证码信息
--(void)DidRerequestToken:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidRerequestToken:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
     [dic setObject:@"IOS" forKey:@"client"];
 
     [self requestMethod:@"POST" serviceName:@"/api.php/verify/phone" parmers:dic completeBlock:^(id obj) {
-        ERROR_CODE code = ERROR_CODE_RROR;
+        ERROR_CODE status = ERROR_CODE_RequestFailed;
         if (obj && obj[@"message"]) {
-            code = [obj[@"code"] intValue];
+            status = [obj[@"status"] intValue];
         }
         
         NSLog(@"%@",obj);
-        callBack(code, obj);
+        callBack(status, obj);
     }];
 }
 //注册
--(void)DidRegister:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidRegister:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
     [dic setObject:@"IOS" forKey:@"client"];
     [self requestMethod:@"POST" serviceName:@"/api.php/user/reg" parmers:dic completeBlock:^(id obj) {
-        ERROR_CODE code = ERROR_CODE_RROR;
+        ERROR_CODE status = ERROR_CODE_RequestFailed;
         if (obj && obj[@"message"]) {
             NSLog(@"%@",obj);
 
-            code = [obj[@"code"] intValue];
+            status = [obj[@"status"] intValue];
         }
-        callBack(code, obj);
+        callBack(status, obj);
     }];
 }
 //获取用户个人信息
--(void)DidUserInfo:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE code, id obj)) callBack
+-(void)DidUserInfo:(NSDictionary*)parmers completion:(void (^)(ERROR_CODE status, id obj)) callBack
 {
     NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:parmers];
     [dic setObject:@"HePd443Mnd" forKey:@"overifyname"];
     [self requestMethod:@"GET" serviceName:@"/cpanel/index.php/buyc_interface1/registerResUser/" parmers:dic completeBlock:^(id obj) {
-        ERROR_CODE code = ERROR_CODE_RROR;
+        ERROR_CODE status = ERROR_CODE_RequestFailed;
         if (obj && obj[@"message"]) {
-            code = [obj[@"code"] intValue];
+            status = [obj[@"status"] intValue];
         }
-        callBack(code, obj);
+        callBack(status, obj);
     }];
 }
 
