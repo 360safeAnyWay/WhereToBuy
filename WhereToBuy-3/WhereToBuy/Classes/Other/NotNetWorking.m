daec55b9012cf0c08d8a08fa6428b9ab53189703
360safeAnyWay <max-mengfanjie@MAX-MENGFANJIEdeMacBook-Pro.local>
2015-04-11 16:08:47 +0800

    在保存下

--- WhereToBuy-3/WhereToBuy/Classes/Other/NotNetWorking.m
+++ WhereToBuy-3/WhereToBuy/Classes/Other/NotNetWorking.m
@@ -14,18 +14,18 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor  = [UIColor darkGrayColor];
+        self.backgroundColor  = [Tools colorWithRed:214 angGreen:214 andBlue:214];
         if (IPhone4) {
             UIImageView * image = [[UIImageView alloc]initWithFrame:CGRectMake(320/2-149/2, 480/2-149/2, 149, 149)];
             image.image = [UIImage imageNamed:@"nowifi@1x.png"];
             [self addSubview:image];
         }else if (IPhone5||IPhone6){
-            UIImageView * image = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
-            image.image = [UIImage imageNamed:@"网络异常.jpg"];
+            UIImageView * image = [[UIImageView alloc]initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width/2-298/2, [UIScreen mainScreen].bounds.size.height/2-298/2, 298, 298)];
+            image.image = [UIImage imageNamed:@"nowifi@2x.png"];
             [self addSubview:image];
         }else{
-            UIImageView * image = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
-            image.image = [UIImage imageNamed:@"网络异常.jpg"];
+            UIImageView * image = [[UIImageView alloc]initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width/2-447/2, [UIScreen mainScreen].bounds.size.height/2-447/2, 447, 447)];
+            image.image = [UIImage imageNamed:@"nowifi@3x.png"];
             [self addSubview:image];
         }
         
